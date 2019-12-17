7002bdb0df8eebd0f067af4a8256d092606d9759
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-12-17 09:33:43 -0500

    display annotation content text by filtering for annotation purpose

--- src/components/Annotations/Annotations.tsx
+++ src/components/Annotations/Annotations.tsx
@@ -4,16 +4,18 @@ interface AnnotationsProps {
     annotations: Array<Object>;
 }
 
-class Annotations extends React.Component <AnnotationsProps> {
+class Annotations extends React.Component<AnnotationsProps> {
     render() {
         const annotation: Array<Object> = []
         for (const annoData of this.props.annotations) {
-          const selector = annoData["target"]["selector"].filter((s) => s["type"] === "TextQuoteSelector")[0]
-          annotation.push(<div key={annoData["id"]} style={{lineHeight: '1.5em',
-              display: 'block', color: 'darkslategray', marginTop: '2em'}}>
-              <blockquote>"...{selector.prefix} <span style={{color: '#3F3730', fontWeight: 'bolder'}}>{selector.exact}</span> {selector.suffix}..."</blockquote>
-              <span style={{fontStyle: "italic"}}>{annoData["body"][0]["value"]}</span>
-          </div>)
+            const selector = annoData["target"]["selector"].filter((s) => s["type"] === "TextQuoteSelector")[0]
+            annotation.push(<div key={annoData["id"]} style={{
+                lineHeight: '1.5em',
+                display: 'block', color: 'darkslategray', marginTop: '2em'
+            }}>
+                <blockquote>"...{selector.prefix} <span style={{ color: '#3F3730', fontWeight: 'bolder' }}>{selector.exact}</span> {selector.suffix}..."</blockquote>
+                <span style={{ fontStyle: "italic" }}>{annoData["body"].filter((a) => a["purpose"] === "commenting")[0]["value"]}</span>
+            </div>)
         }
         return <>{annotation}</>
     }
